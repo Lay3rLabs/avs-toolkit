bc0a8c24b170fcb6fa20aa998b6b243460f92f0f
David Komer <6406986+dakom@users.noreply.github.com>
2024-09-25 13:19:47 +0300

    fmt

--- packages/apis/src/id.rs
+++ packages/apis/src/id.rs
@@ -46,7 +46,7 @@ impl<'a> Prefixer<'a> for TaskId {
 
 impl KeyDeserialize for TaskId {
     type Output = TaskId;
-    const KEY_ELEMS: u16 = 1; 
+    const KEY_ELEMS: u16 = 1;
 
     #[inline(always)]
     fn from_vec(value: Vec<u8>) -> StdResult<Self::Output> {
