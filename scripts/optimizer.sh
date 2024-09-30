62cbe33bb22ac3fde055ce59efd3324a905d7b21
ismellike <glopez11193@gmail.com>
2024-09-30 10:43:17 -0500

    Update optimizer.sh
    Only check $SSH_AUTH_SOCK when $OSTYPE is not 'darwin', because this is when the var is used.

--- scripts/optimizer.sh
+++ scripts/optimizer.sh
@@ -18,13 +18,6 @@ A="linux/${M/x86_64/amd64}"
 S=${M#x86_64}
 S=${S:+-$S}
 
-# Ensure SSH agent is running and SSH_AUTH_SOCK is available
-if [ -z "$SSH_AUTH_SOCK" ]; then
-  echo "SSH_AUTH_SOCK is not set. Starting ssh-agent..."
-  eval $(ssh-agent)
-  echo "SSH agent started with SSH_AUTH_SOCK=$SSH_AUTH_SOCK"
-fi
-
 # We pass our ssh agent to the docker container, so it can pull dev dependencies properly
 if [[ $OSTYPE == 'darwin'* ]]; then
   $SUDO docker run --platform $A --rm -v "$(pwd)":/code \
@@ -33,6 +26,13 @@ if [[ $OSTYPE == 'darwin'* ]]; then
   -v /run/host-services/ssh-auth.sock:/run/host-services/ssh-auth.sock -e SSH_AUTH_SOCK="/run/host-services/ssh-auth.sock" \
   $U/optimizer$S:$V
 else
+  # Ensure SSH agent is running and SSH_AUTH_SOCK is available
+  if [ -z "$SSH_AUTH_SOCK" ]; then
+    echo "SSH_AUTH_SOCK is not set. Starting ssh-agent..."
+    eval $(ssh-agent)
+    echo "SSH agent started with SSH_AUTH_SOCK=$SSH_AUTH_SOCK"
+  fi
+
   $SUDO docker run --platform $A --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
