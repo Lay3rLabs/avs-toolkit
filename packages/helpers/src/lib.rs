edc450076caec0fbf5b5f491c86f74d35c817d42
Kaloyan Gangov <6922910+gangov@users.noreply.github.com>
2024-10-10 15:17:42 +0300

    adds Nanos as duration measurement

--- packages/helpers/src/lib.rs
+++ packages/helpers/src/lib.rs
@@ -1,1 +1,21 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::Uint64;
+
 pub mod verifier;
+
+/// A wrapper around u64, that represents duration between two points in time.
+#[cw_serde]
+#[derive(Copy, PartialOrd, Ord, Eq)]
+pub struct Nanos(Uint64);
+
+impl Nanos {
+    /// Create a new `Nanos` from `u64` value.
+    pub fn new(input: u64) -> Self {
+        Self(input.into())
+    }
+
+    /// Get a copy of the internal data
+    pub fn u64(self) -> u64 {
+        self.0.u64()
+    }
+}
