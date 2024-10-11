3dde5e61b6869a447a9a97b7bb1049e98287599a
Kaloyan Gangov <6922910+gangov@users.noreply.github.com>
2024-10-11 17:51:49 +0300

    adds unit tests

--- packages/apis/src/time.rs
+++ packages/apis/src/time.rs
@@ -31,3 +31,77 @@ impl std::fmt::Display for Duration {
         write!(f, "{}", self.0)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json;
+
+    #[test]
+    fn duration_serialization() {
+        // 123 seconds/nanos with precision
+        let duration_nanos = Duration::new_nanos(1_234_567_890);
+        let duration_seconds = Duration::new_seconds(123);
+
+        let serialized_nanos = serde_json::to_string(&duration_nanos).unwrap();
+        let serialized_seconds = serde_json::to_string(&duration_seconds).unwrap();
+
+        // seconds has a lot of `0` because of the way it is serialized
+        assert_eq!(serialized_nanos, "\"1234567890\"");
+        assert_eq!(serialized_seconds, "\"123000000000\"");
+
+        let deserialized_nanos: Duration = serde_json::from_str(&serialized_nanos).unwrap();
+        let deserialized_seconds: Duration = serde_json::from_str(&serialized_seconds).unwrap();
+
+        assert_eq!(deserialized_nanos.as_nanos(), 1_234_567_890);
+        assert_eq!(deserialized_seconds.as_nanos(), 123_000_000_000);
+        assert_eq!(deserialized_seconds.as_seconds(), 123);
+    }
+
+    #[test]
+    fn duration_zero_serialization() {
+        let duration_zero = Duration::new_nanos(0);
+        let serialized_zero = serde_json::to_string(&duration_zero).unwrap();
+        assert_eq!(serialized_zero, "\"0\"");
+
+        let deserialized_zero: Duration = serde_json::from_str(&serialized_zero).unwrap();
+        assert_eq!(deserialized_zero.as_nanos(), 0);
+    }
+
+    #[test]
+    fn duration_max_value_serialization() {
+        let duration_max = Duration::new_nanos(u64::MAX);
+        let serialized_max = serde_json::to_string(&duration_max).unwrap();
+        assert_eq!(serialized_max, format!("\"{}\"", u64::MAX));
+
+        let deserialized_max: Duration = serde_json::from_str(&serialized_max).unwrap();
+        assert_eq!(deserialized_max.as_nanos(), u64::MAX);
+        assert_eq!(deserialized_max.as_seconds(), u64::MAX / NANOS_PER_SECONDS);
+    }
+
+    #[test]
+    fn duration_methods() {
+        let duration = Duration::new_seconds(2);
+        assert_eq!(duration.as_nanos(), 2_000_000_000);
+        assert_eq!(duration.as_seconds(), 2);
+
+        let duration_nanos = Duration::new_nanos(2_500_000_000);
+        assert_eq!(duration_nanos.as_nanos(), 2_500_000_000);
+        // it'll round down
+        assert_eq!(duration_nanos.as_seconds(), 2);
+    }
+
+    #[test]
+    fn duration_equality() {
+        let duration1 = Duration::new_nanos(1_000_000_000);
+        let duration2 = Duration::new_seconds(1);
+        assert_eq!(duration1, duration2);
+    }
+
+    #[test]
+    fn duration_ordering() {
+        let duration_short = Duration::new_nanos(1_000_000_000);
+        let duration_long = Duration::new_nanos(2_000_000_000);
+        assert!(duration_short < duration_long);
+    }
+}
