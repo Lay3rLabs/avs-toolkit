dcb7ae8ba784f34bc111dd3e67422ea751dcd0c4
Kaloyan Gangov <6922910+gangov@users.noreply.github.com>
2024-10-15 15:52:08 +0300

    reverts the builder pattern

--- packages/helpers/src/events.rs
+++ packages/helpers/src/events.rs
@@ -1,5 +1,5 @@
+use cosmwasm_std::StdError;
 use cosmwasm_std::{Attribute, Event};
-use cosmwasm_std::{Decimal, StdError};
 use lavs_apis::id::TaskId;
 
 pub trait TypedEvent: TryFrom<Event> + Into<Event> {
@@ -10,110 +10,11 @@ pub trait TypedEvent: TryFrom<Event> + Into<Event> {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum TaskEvent {
-    Executed {
-        task_id: TaskId,
-        task_queue: String,
-        operator: String,
-        completed: bool,
-    },
-    VoteStored {
-        task_id: TaskId,
-        task_queue_contract: String,
-        new_price: Decimal,
-    },
-    ThresholdNotMet {
-        task_id: TaskId,
-        task_queue_contract: String,
-    },
-}
-
-#[derive(Debug, Clone, PartialEq)]
 pub struct TaskExecutedEvent {
     pub task_id: TaskId,
     pub task_queue: String,
-    pub operator: Option<String>,
-    pub completed: Option<bool>,
-    pub method: Option<String>,
-    pub status: Option<String>,
-    pub new_price: Option<Decimal>,
-    pub action: Option<String>,
-}
-
-#[derive(Default)]
-pub struct TaskExecutedEventBuilder {
-    // Fields corresponding to TaskExecutedEvent
-    task_id: Option<TaskId>,
-    task_queue: Option<String>,
-    operator: Option<String>,
-    completed: Option<bool>,
-    method: Option<String>,
-    status: Option<String>,
-    new_price: Option<Decimal>,
-    action: Option<String>,
-}
-
-impl TaskExecutedEventBuilder {
-    pub fn new() -> Self {
-        Self::default()
-    }
-
-    pub fn task_id(mut self, task_id: TaskId) -> Self {
-        self.task_id = Some(task_id);
-        self
-    }
-
-    pub fn task_queue(mut self, task_queue: String) -> Self {
-        self.task_queue = Some(task_queue);
-        self
-    }
-
-    pub fn operator(mut self, operator: String) -> Self {
-        self.operator = Some(operator);
-        self
-    }
-
-    pub fn completed(mut self, completed: bool) -> Self {
-        self.completed = Some(completed);
-        self
-    }
-
-    pub fn method(mut self, method: String) -> Self {
-        self.method = Some(method);
-        self
-    }
-
-    pub fn status(mut self, status: String) -> Self {
-        self.status = Some(status);
-        self
-    }
-
-    pub fn new_price(mut self, new_price: Decimal) -> Self {
-        self.new_price = Some(new_price);
-        self
-    }
-
-    pub fn action(mut self, action: String) -> Self {
-        self.action = Some(action);
-        self
-    }
-
-    pub fn build(self) -> Result<TaskExecutedEvent, StdError> {
-        Ok(TaskExecutedEvent {
-            task_id: self
-                .task_id
-                .ok_or_else(|| StdError::generic_err("task_id is required"))?,
-            task_queue: self
-                .task_queue
-                .ok_or_else(|| StdError::generic_err("task_queue is required"))?,
-            operator: self.operator,
-            completed: self.completed,
-            method: self.method,
-            status: self.status,
-            new_price: self.new_price,
-            action: self.action,
-        })
-    }
+    pub operator: String,
+    pub completed: bool,
 }
 
 impl TypedEvent for TaskExecutedEvent {
@@ -132,45 +33,49 @@ impl TryFrom<&Event> for TaskExecutedEvent {
             )));
         }
 
-        let mut builder = TaskExecutedEventBuilder::new();
+        let mut task_id: Option<TaskId> = None;
+        let mut task_queue: Option<String> = None;
+        let mut operator: Option<String> = None;
+        let mut completed: Option<bool> = None;
 
         for Attribute { key, value } in event.attributes.iter() {
             match key.as_str() {
                 "task-id" => {
-                    builder = builder.task_id(value.parse().map_err(|_| {
-                        StdError::generic_err(format!("Invalid value for task-id: {}", value))
-                    })?);
+                    if let Ok(value) = value.parse() {
+                        task_id = Some(value);
+                    }
                 }
                 "task-queue" => {
-                    builder = builder.task_queue(value.clone());
+                    if let Ok(value) = value.parse() {
+                        task_queue = Some(value);
+                    }
                 }
                 "operator" => {
-                    builder = builder.operator(value.clone());
+                    if let Ok(value) = value.parse() {
+                        operator = Some(value);
+                    }
                 }
                 "completed" => {
-                    builder = builder.completed(value.parse().map_err(|_| {
-                        StdError::generic_err(format!("Invalid value for completed: {}", value))
-                    })?);
-                }
-                "method" => {
-                    builder = builder.method(value.clone());
-                }
-                "status" => {
-                    builder = builder.status(value.clone());
-                }
-                "new_price" => {
-                    builder = builder.new_price(value.parse().map_err(|_| {
-                        StdError::generic_err(format!("Invalid value for new_price: {}", value))
-                    })?);
-                }
-                "action" => {
-                    builder = builder.action(value.clone());
+                    if let Ok(value) = value.parse() {
+                        completed = Some(value)
+                    }
                 }
                 _ => {}
             }
         }
 
-        builder.build()
+        match (task_id, task_queue, operator, completed) {
+            (Some(task_id), Some(task_queue), Some(operator), Some(completed)) => Ok(Self {
+                task_id,
+                task_queue,
+                operator,
+                completed,
+            }),
+            _ => Err(StdError::generic_err(format!(
+                "Could not parse fields for {}",
+                Self::NAME,
+            ))),
+        }
     }
 }
 
@@ -184,38 +89,11 @@ impl TryFrom<Event> for TaskExecutedEvent {
 
 impl From<TaskExecutedEvent> for Event {
     fn from(value: TaskExecutedEvent) -> Self {
-        let mut event = Event::new(TaskExecutedEvent::NAME);
-
-        event = event.add_attributes(vec![
-            Attribute {
-                key: "task-id".to_string(),
-                value: value.task_id.to_string(),
-            },
-            Attribute {
-                key: "task-queue".to_string(),
-                value: value.task_queue,
-            },
-        ]);
-
-        if let Some(operator) = value.operator {
-            event = event.add_attribute("operator", operator);
-        }
-        if let Some(completed) = value.completed {
-            event = event.add_attribute("completed", completed.to_string());
-        }
-        if let Some(method) = value.method {
-            event = event.add_attribute("method", method);
-        }
-        if let Some(status) = value.status {
-            event = event.add_attribute("status", status);
-        }
-        if let Some(new_price) = value.new_price {
-            event = event.add_attribute("new_price", new_price.to_string());
-        }
-        if let Some(action) = value.action {
-            event = event.add_attribute("action", action);
-        }
-
-        event
+        Self::new(TaskExecutedEvent::NAME).add_attributes([
+            ("task-id", value.task_id.to_string()),
+            ("task-queue", value.task_queue.to_string()),
+            ("operator", value.operator.to_string()),
+            ("completed", value.completed.to_string()),
+        ])
     }
 }
