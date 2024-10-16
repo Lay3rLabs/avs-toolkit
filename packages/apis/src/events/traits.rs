c75686fe5a7e69513fd00edc44212293d9a33b32
Kaloyan Gangov <6922910+gangov@users.noreply.github.com>
2024-10-16 11:57:05 +0300

    fixes a failing test

--- packages/apis/src/events/traits.rs
+++ packages/apis/src/events/traits.rs
@@ -3,6 +3,6 @@ use cosmwasm_std::Event;
 pub trait TypedEvent: TryFrom<Event> + Into<Event> {
     const NAME: &'static str;
     fn is_type(ty: &str) -> bool {
-        Self::NAME == ty || Self::NAME == format!("wasm-{ty}")
+        ty == Self::NAME || ty == format!("wasm-{}", Self::NAME)
     }
 }
